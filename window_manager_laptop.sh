d51a02a5ee4abf77e7b7981a8318083d51ceb2aa
Austin Wasson <austinpwasson@gmail.com>
2023-12-01 16:01:32 -0700

    adjust laptop

--- window_manager_laptop.sh
+++ window_manager_laptop.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
 osascript -e '
-tell application "Warp" to activate
-
-tell application "System Events" to click UI element "Warp" of list 1 of application process "Dock"
-
-tell application "System Events" to keystroke " " using command down
 
 tell application "System Events"
-    delay .1
-    keystroke "first third"
-    delay .1
-    key code 36
-end tell
-
-delay .1
-
-tell application "Visual Studio Code" to activate
-
-tell application "System Events" to click UI element "Visual Studio Code" of list 1 of application process "Dock"
-
-tell application "System Events" to keystroke " " using command down
-
-tell application "System Events"
-    delay .1
-    keystroke "last two thirds"
-    delay .1
-    key code 36
+    tell process "Visual Studio Code"
+        set frontmost to true
+        delay 1
+        keystroke (ASCII character 29) using {control down, shift down}
+    end tell
 end tell
 '
